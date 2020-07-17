5e4a7611c6110558372a6a2c3aa703acf547e53c
lukaaspl <lukaaspl@gmail.com>
2020-07-18 00:12:09 +0200

    add http-error

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app: express.Application = express();
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Hi!");
---
+++ src/models/http-error.ts
@@ -0,0 +1,7 @@
+class HttpError extends Error {
+  constructor(message: string, private code: number) {
+    super(message);
+  }
+}
+
+export default HttpError;
